66b3864d7e691c61e7c02e07e27a5fa20120b351
Zhou Zihan <2510791Z@student.gla.ac.uk>
2022-10-27 12:12:06 +0800

    ✨ feat: add challenge

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -499,12 +499,37 @@ contract Campaign is ICampaign, Ownable, ERC721 {
   }
 
   modifier onlyChallengeExist(uint256 challengeRecordId) {
-    require(challengeRecordId < _challengeIdx, 'Campaign: challenge record not exist');
+    require(challengeRecordId < _challengeIdx, 'ChallengeRecord: not exist');
     _;
   }
 
   modifier onlyChallengeNotEnded(uint256 challengeRecordId) {
-    require(block.timestamp < challengeRecords[challengeRecordId].challengeRiseTime + 7 days);
+    require(block.timestamp < challengeRecords[challengeRecordId].challengeRiseTime + 7 days, 'Challenge: ended');
+    _;
+  }
+
+  modifier onlyChallengeEnded(uint256 challengeRecordId) {
+    require(block.timestamp >= challengeRecords[challengeRecordId].challengeRiseTime + 7 days,  'Challenge: not ended');
+    _;
+  }
+
+  modifier onlyChallengeAllowed() {
+    require(block.timestamp <= startTime + totalEpochsCount * period + 1 days, 'Challenge: not allowed');
+    _;
+  }
+
+  modifier onlyNotJudged(uint256 challengeRecordId) {
+    require(challengeRecords[challengeRecordId].state == false, 'Challenge: already judged');
+    _;
+  }
+
+  modifier onlyAllJudged() {
+    require(_challengeIdx == challengeJudgedCount, 'Challenge: not all judged');
+    _;
+  }
+
+  modifier onlyEnoughCheater() {
+    require(cheatCount >= _idx * 3/10, 'Campaign: not enough cheater');
     _;
   }
 }
